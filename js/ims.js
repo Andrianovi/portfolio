09996a99ea94cc0f5fa127d68d551b7d64cf8414
Andrianovi <106331579+Andrianovi@users.noreply.github.com>
2025-05-26 07:29:59 +0500

    Add files via upload

---
+++ js/ims.js
@@ -0,0 +1,85 @@
+window.addEventListener('load', function () {
+	//*@ замена адресов для ссылок поделиться
+
+	let addressURL = window.location.href;
+	let siteName = `Вёрстка сайтов`;
+	let siteTitle = 'Адаптив под любой размер экрана, оптимизация под слабый интернет, анимация движений объектов, изменения размеров, надёжная вёрстка';
+
+
+
+	let socialVk = document.querySelector('a.social-vk');
+	let socialTg = document.querySelector('a.social-tg');
+	let socialOk = document.querySelector('a.social-ok');
+	let socialWts = document.querySelector('a.social-wts');
+	// let socialFb = document.querySelector('a.social-fb');
+	// let socialTwit = document.querySelector('a.social-twit');
+
+
+	socialVk.setAttribute('href', `https://vk.com/share.php?url=${addressURL}`);
+	socialTg.setAttribute('href', `https://telegram.me/share/url?url=${addressURL}&text=${siteName}`);
+	socialOk.setAttribute('href', `https://connect.ok.ru/offer?url=${addressURL}`);
+	socialWts.setAttribute('href', `https://api.whatsapp.com/send?text=${addressURL}`);
+	// socialFb.setAttribute('href', `https://www.facebook.com/sharer/sharer.php?u=${addressURL}`);
+	// socialTwit.setAttribute('href', `https://twitter.com/intent/tweet?text=${siteName}&url=${addressURL}&via=${siteTitle}`);
+
+
+
+	let imagesHidden = document.querySelectorAll('.img-hidden');
+
+	/* 	imagesHidden.forEach(i => {
+			i.style.display = 'block';
+		});
+		*/
+
+
+
+
+
+
+
+
+	/* function waitForElement(elementId, callBack) {
+		window.setTimeout(function () {
+			var element = document.getElementById(elementId);
+			if (element) {
+				callBack(elementId, element);
+			} else {
+				waitForElement(elementId, callBack);
+			}
+		}, 500)
+	} */
+
+	/* waitForElement('slide1-img', function () {
+		console.log('Ghbstn');
+
+		const fonts = [
+			new FontFace("Montserrat", 'url(./fonts/Montserrat-Bold.woff2)', {
+				weight: "700",
+				stretch: "condensed",
+			}),
+			new FontFace("Montserrat", 'url(./fonts/Montserrat-SemiBold.woff2)', {
+				weight: "600",
+				stretch: "condensed",
+			}),
+			new FontFace("Montserrat", 'url(./fonts/Montserrat-Medium.woff2)', {
+				weight: "500",
+				stretch: "condensed",
+			}),
+			new FontFace("Gilroy", 'url(./fonts/Gilroy-Semibold.woff2)', {
+				weight: "600",
+				stretch: "condensed",
+			}),
+		];
+		fonts.forEach(fontFace => {
+			document.fonts.add(fontFace);
+		});
+
+
+	}); */
+
+
+
+
+
+
+});
